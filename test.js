fc25fcfead228b6b743d3f78fd1eee1326087bd3
louissanchez84 <louissanchez84@gmail.com>
2021-04-17 14:48:07 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+{
+ _isReactElement: true,
+ _store: {},
+ type: "body",
+ props: {
+   dangerouslySetInnerHTML: {
+     __html:
+     "<h1>Arbitrary HTML</h1>
+     <script>alert(‘No CSP Support :(')</script>
+     <a href='http://danlec.com'>link</a>"
+    }
+  }
+}
